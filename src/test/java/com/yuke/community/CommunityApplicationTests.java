27dfdba6d77f468ca32d776b151cc12fd9136417
developerYuke <developer_yuke@aliyun.com>
2020-08-22 06:54:01 +0800

    "init"

---
+++ src/test/java/com/yuke/community/CommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yuke.community;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
